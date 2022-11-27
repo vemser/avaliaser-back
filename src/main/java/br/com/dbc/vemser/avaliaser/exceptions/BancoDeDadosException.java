2c6b40409a7e5e56de3ad85192e228233b094128
paulo-pssj <paulinhops99@gmail.com>
2022-11-27 15:41:58 -0300

    update: add exceptions

---
+++ src/main/java/br/com/dbc/vemser/avaliaser/exceptions/BancoDeDadosException.java
@@ -0,0 +1,8 @@
+package br.com.dbc.vemser.avaliaser.exceptions;
+
+public class BancoDeDadosException extends Exception {
+
+    public BancoDeDadosException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/dbc/vemser/avaliaser/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package br.com.dbc.vemser.avaliaser.exceptions;
+
+public class RegraDeNegocioException extends Exception {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
