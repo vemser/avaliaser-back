0a1d53539d672bcbcdd8c60a8d82f54e327d4d66
paulo-pssj <paulinhops99@gmail.com>
2022-11-27 21:12:28 -0300

    update: add dependencia do Mockito, limpando e organizando codigo

--- src/main/java/br/com/dbc/vemser/avaliaser/controllers/UsuarioController.java
+++ src/main/java/br/com/dbc/vemser/avaliaser/controllers/UsuarioController.java
@@ -4,11 +4,9 @@ import br.com.dbc.vemser.avaliaser.dto.login.LoginDTO;
 import br.com.dbc.vemser.avaliaser.dto.login.UsuarioCreateDTO;
 import br.com.dbc.vemser.avaliaser.dto.login.UsuarioDTO;
 import br.com.dbc.vemser.avaliaser.dto.login.UsuarioLogadoDTO;
-import br.com.dbc.vemser.avaliaser.entities.UsuarioEntity;
 import br.com.dbc.vemser.avaliaser.enums.Cargo;
+import br.com.dbc.vemser.avaliaser.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.avaliaser.services.UsuarioService;
-import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,7 +16,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -32,23 +29,22 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
 
-
     @GetMapping("/listar-usuarios")
-    public ResponseEntity<List<UsuarioLogadoDTO>> listarUsuario() throws IOException {
+    public ResponseEntity<List<UsuarioDTO>> listarUsuario() {
         log.info("Retornando Usuário logado...");
-        List<UsuarioLogadoDTO> usuario = usuarioService.findAll();
+        List<UsuarioDTO> usuario = usuarioService.findAll();
         log.info("Retorno de usuário logado com sucesso.");
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
-    @GetMapping("/usuarioById/{idUsuario}")
-    public ResponseEntity<UsuarioLogadoDTO> getUsuarioById(@PathVariable Integer idUsuario) throws IOException {
-        UsuarioLogadoDTO usuario = usuarioService.findByIdDTO(idUsuario);
+    @GetMapping("/usuario/{idUsuario}")
+    public ResponseEntity<UsuarioDTO> getUsuarioById(@PathVariable Integer idUsuario) throws RegraDeNegocioException {
+        UsuarioDTO usuario = usuarioService.findByIdDTO(idUsuario);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
     @GetMapping("/usuario-logado")
-    public ResponseEntity<UsuarioLogadoDTO> getUsuarioLogado() throws IOException {
+    public ResponseEntity<UsuarioLogadoDTO> getUsuarioLogado() throws RegraDeNegocioException {
         log.info("Retornando Usuário logado...");
         UsuarioLogadoDTO usuario = usuarioService.getUsuarioLogado();
         log.info("Retorno de usuário logado com sucesso.");
@@ -65,34 +61,29 @@ public class UsuarioController {
 
     @PostMapping(value = "/cadastrar-usuario")
     public ResponseEntity<UsuarioDTO> cadastrarUsuario(@RequestParam Cargo cargo,
-                                                             @RequestBody UsuarioCreateDTO usuarioCreateDTO) throws IOException{
-    UsuarioDTO usuarioLogadoDTO = usuarioService.cadastrarUsuario(usuarioCreateDTO, cargo);
-        return new ResponseEntity<>(usuarioLogadoDTO,HttpStatus.OK);
-}
+                                                       @RequestBody UsuarioCreateDTO usuarioCreateDTO){
+        UsuarioDTO usuarioLogadoDTO = usuarioService.cadastrarUsuario(usuarioCreateDTO, cargo);
+        return new ResponseEntity<>(usuarioLogadoDTO, HttpStatus.OK);
+    }
+
     @PutMapping(value = "/upload-imagem/{idUsuario}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<UsuarioLogadoDTO> uploadImagem(@RequestPart(value = "file") MultipartFile file,
-                                                         @RequestParam(value = "idUsuario") Integer idUsuario) throws IOException {
-            UsuarioLogadoDTO usuarioLogadoDTO = usuarioService.uploadImagem(file, idUsuario);
-            return new ResponseEntity<>(usuarioLogadoDTO, HttpStatus.OK);
+    public ResponseEntity<UsuarioLogadoDTO> uploadImagem(@RequestPart(value = "file", required = false) MultipartFile file,
+                                                         @RequestParam(value = "idUsuario") Integer idUsuario) throws RegraDeNegocioException {
+        UsuarioLogadoDTO usuarioLogadoDTO = usuarioService.uploadImagem(file, idUsuario);
+        return new ResponseEntity<>(usuarioLogadoDTO, HttpStatus.OK);
     }
 
-//    @PutMapping(value ="/atualizar-usuario-logado", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-//    public ResponseEntity<UsuarioLogadoDTO> updateUsuario(@RequestPart(value = "file") MultipartFile file,
-//                                                          @Valid @RequestBody UsuarioCreateDTO usuarioCreateDTO) throws IOException {
-//        return new ResponseEntity<>(usuarioService.atualizarUsuarioLogado(file, usuarioCreateDTO), HttpStatus.OK);
-//    }
 
-    @PutMapping(value ="/atualizar-usuario-byId/{idUsuario}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<UsuarioLogadoDTO> updateUsuarioById(@RequestPart(value = "file") MultipartFile file,
-                                                              @RequestParam String nome,
+    @PutMapping(value = "/atualizar-usuario/{idUsuario}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UsuarioLogadoDTO> updateUsuarioById(@RequestParam String nome,
                                                               @RequestParam String email,
-                                                              @PathVariable Integer idUsuario ) throws IOException {
+                                                              @PathVariable Integer idUsuario) throws RegraDeNegocioException {
         UsuarioCreateDTO usuarioCreateDTO = new UsuarioCreateDTO(email, nome);
-        return new ResponseEntity<>(usuarioService.atualizarUsuario(file, usuarioCreateDTO, idUsuario), HttpStatus.OK);
+        return new ResponseEntity<>(usuarioService.atualizarUsuario(usuarioCreateDTO, idUsuario), HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete-usuarioByID/{idUsuario}")
-    public ResponseEntity<Void> deletarUsuarioCliente(@PathVariable Integer idUsuario) throws IOException {
+    @DeleteMapping("/delete/{idUsuario}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Integer idUsuario) throws RegraDeNegocioException {
         usuarioService.desativarUsuarioById(idUsuario);
         return new ResponseEntity<>(HttpStatus.OK);
     }
