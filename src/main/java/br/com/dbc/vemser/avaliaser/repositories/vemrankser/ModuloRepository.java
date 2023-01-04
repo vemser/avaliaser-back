a8f1a49f9a98dcae26c2e13f72acd87a32dbc1bf
JeanThomaz <jeanlucas.tjc@gmail.com>
2023-01-04 14:29:02 -0300

    update: endpoint para listar os modulos por id ou por nome

--- src/main/java/br/com/dbc/vemser/avaliaser/repositories/vemrankser/ModuloRepository.java
+++ src/main/java/br/com/dbc/vemser/avaliaser/repositories/vemrankser/ModuloRepository.java
@@ -1,15 +1,19 @@
 package br.com.dbc.vemser.avaliaser.repositories.vemrankser;
 
 import br.com.dbc.vemser.avaliaser.entities.ModuloEntity;
+import br.com.dbc.vemser.avaliaser.entities.TecnologiaEntity;
 import br.com.dbc.vemser.avaliaser.enums.Ativo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ModuloRepository extends JpaRepository<ModuloEntity, Integer> {
 
+    Page<ModuloEntity> findAllByNomeContainingIgnoreCase(Pageable pageable, String nome);
+    Page<ModuloEntity> findAllByIdModulo(Pageable pageable, Integer id);
     Optional<ModuloEntity> findByIdModuloAndAtivo(Integer idModulo, Ativo ativo);
     Page<ModuloEntity> findAllByAtivo(Ativo ativo, Pageable pageable);
 
