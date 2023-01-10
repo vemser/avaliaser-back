45fb7cd50df5895f4fc25b51ec46164e945b3a48
Felipe-Noguez <felipenoguez@outlook.com.br>
2023-01-10 14:13:11 -0300

    Correção de BUG no cadastro de módulo (trilha/programa não pode ser nulo)

--- src/main/java/br/com/dbc/vemser/avaliaser/dto/vemrankser/modulodto/ModuloCreateDTO.java
+++ src/main/java/br/com/dbc/vemser/avaliaser/dto/vemrankser/modulodto/ModuloCreateDTO.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -16,10 +17,12 @@ public class ModuloCreateDTO {
     @Schema(description = "nome do modulo", example = "OOP")
     private String nome;
 
+    @NotEmpty(message = "Campo não pode estar em branco ou nulo!")
     @NotNull(message = "Campo não pode estar em branco ou nulo!")
     @Schema(example = "[1, 2, 3]")
     private List<Integer> listPrograma;
 
+    @NotEmpty(message = "Campo não pode estar em branco ou nulo!")
     @Schema(example = "[1, 2, 3]")
     private List<Integer> trilha;
 
