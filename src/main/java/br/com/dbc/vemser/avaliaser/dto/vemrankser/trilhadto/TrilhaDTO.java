2fa70f8b7ff0ade84ebc5ff9eac52f762c066c12
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2023-01-12 17:06:06 -0300

    teste unitario avaliacao

--- src/main/java/br/com/dbc/vemser/avaliaser/dto/vemrankser/trilhadto/TrilhaDTO.java
+++ src/main/java/br/com/dbc/vemser/avaliaser/dto/vemrankser/trilhadto/TrilhaDTO.java
@@ -3,11 +3,13 @@ package br.com.dbc.vemser.avaliaser.dto.vemrankser.trilhadto;
 import br.com.dbc.vemser.avaliaser.dto.vemrankser.modulodto.ModuloDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrilhaDTO  {
 
     private Integer idTrilha;
